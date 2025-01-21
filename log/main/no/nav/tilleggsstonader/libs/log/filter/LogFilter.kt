62b2ec1a9b02628fa5587ce15da7cbeaae8bbabd
Johan Blomgren <blommish@users.noreply.github.com>
2025-01-21 09:58:00 +0100

    Legger til at man må konfigurere at man skal spore bruker, som default er disabled

--- log/main/no/nav/tilleggsstonader/libs/log/filter/LogFilter.kt
+++ log/main/no/nav/tilleggsstonader/libs/log/filter/LogFilter.kt
@@ -15,24 +15,20 @@ import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import java.io.EOFException
 
-class LogFilter : HttpFilter() {
+/**
+ * @param sporBruker settes hvis vi skal sette cookie som sporer brukeren for å hjelpe ved feilsøing
+ */
+class LogFilter(val sporBruker: Boolean) : HttpFilter() {
 
     override fun doFilter(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain,
     ) {
-        val userId = resolveUserId(request)
-        if (userId.isNullOrEmpty()) {
-            // user-id tracking only works if the client is stateful and supports cookies.
-            // if no user-id is found, generate one for any following requests but do not use it on the
-            // current request to avoid generating large numbers of useless user-ids.
-            generateUserIdCookie(response)
-        }
+        sporBruker(request, response)
         val consumerId = request.getHeader(NavHttpHeaders.NAV_CONSUMER_ID.asString())
         val callId = resolveCallId(request)
         MDC.put(MDC_CALL_ID, callId)
-        MDC.put(MDC_USER_ID, userId)
         MDC.put(MDC_CONSUMER_ID, consumerId)
         MDC.put(MDC_REQUEST_ID, resolveRequestId(request))
         response.setHeader(NavHttpHeaders.NAV_CALL_ID.asString(), callId)
@@ -45,6 +41,23 @@ class LogFilter : HttpFilter() {
             MDC.remove(MDC_CONSUMER_ID)
             MDC.remove(MDC_REQUEST_ID)
         }
+    }
+
+    private fun sporBruker(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+    ) {
+        if (!sporBruker) {
+            return
+        }
+        val userId = resolveUserId(request)
+        if (userId.isNullOrEmpty()) {
+            // user-id tracking only works if the client is stateful and supports cookies.
+            // if no user-id is found, generate one for any following requests but do not use it on the
+            // current request to avoid generating large numbers of useless user-ids.
+            generateUserIdCookie(response)
+        }
+        MDC.put(MDC_USER_ID, userId)
     }
 
     private fun filterWithErrorHandling(
--- log/main/no/nav/tilleggsstonader/libs/log/filter/LogFilterConfiguration.kt
+++ log/main/no/nav/tilleggsstonader/libs/log/filter/LogFilterConfiguration.kt
@@ -1,12 +1,16 @@
 package no.nav.tilleggsstonader.libs.log.filter
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class LogFilterConfiguration {
+class LogFilterConfiguration(
+    @Value("\${log.sporBruker:false}")
+    private val sporBruker: Boolean,
+) {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
@@ -14,7 +18,7 @@ class LogFilterConfiguration {
     fun logFilter(): FilterRegistrationBean<LogFilter> {
         logger.info("Registering LogFilter filter")
         val filterRegistration = FilterRegistrationBean<LogFilter>()
-        filterRegistration.filter = LogFilter()
+        filterRegistration.filter = LogFilter(sporBruker = sporBruker)
         filterRegistration.order = 1
         return filterRegistration
     }
