b2d91c7193d7b262ce110cfd683a0a9b24f05e86
Andreas Strand <andreas.strand@nav.no>
2026-08-03 22:27:38 +0200

    Endrer prefix på versions-plugin da den har blitt endret og logger warning, samt bump versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
     `java-library`
     id("com.diffplug.spotless") version "8.6.0"
-    id("com.github.ben-manes.versions") version "0.54.0"
+    id("io.github.ben-manes.versions") version "0.59.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
 }
 
@@ -21,7 +21,7 @@ allprojects {
     }
 
     apply(plugin = "com.diffplug.spotless")
-    apply(plugin = "com.github.ben-manes.versions")
+    apply(plugin = "io.github.ben-manes.versions")
     apply(plugin = "se.patrikerdes.use-latest-versions")
     spotless {
         kotlin {
